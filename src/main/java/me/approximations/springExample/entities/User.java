b25f039f9655ade72eb6f8ad4372dbfbec118e0d
Approximations <datkidbosst@gmail.com>
2024-02-07 20:06:47 -0300

    feat: make User a JPA entity

--- src/main/java/me/approximations/springExample/entities/User.java
+++ src/main/java/me/approximations/springExample/entities/User.java
@@ -1,5 +1,9 @@
 package me.approximations.springExample.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import java.time.Instant;
 @Data
 @NoArgsConstructor(force=true)
 @AllArgsConstructor
+@Entity(name="se_users")
 public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private final Long id;
     private String name;
     private String email;
