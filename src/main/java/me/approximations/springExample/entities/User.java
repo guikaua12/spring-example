bd1319d5121d118ed4060e1c9073b1be8eb779fc
Approximations <datkidbosst@gmail.com>
2024-02-08 14:56:38 -0300

    feat: make User implements Serializable

--- src/main/java/me/approximations/springExample/entities/User.java
+++ src/main/java/me/approximations/springExample/entities/User.java
@@ -8,13 +8,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.Instant;
 
 @Data
 @NoArgsConstructor(force=true)
 @AllArgsConstructor
 @Entity(name="se_users")
-public class User {
+public class User implements Serializable {
+    @Serial private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private final Long id;
