43f9bb0afd41fbef4c4e64c21d96e2500f627305
Approximations <datkidbosst@gmail.com>
2024-02-07 21:22:32 -0300

    feat: add UserService

--- src/main/java/me/approximations/springExample/controllers/UserController.java
+++ src/main/java/me/approximations/springExample/controllers/UserController.java
@@ -1,21 +1,35 @@
 package me.approximations.springExample.controllers;
 
+import lombok.RequiredArgsConstructor;
 import me.approximations.springExample.entities.User;
+import me.approximations.springExample.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
+    private final UserService userService;
+
     @RequestMapping(method=RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers() {
-        final User user = new User(1L, "Approximations", "email@approximations.software", "+1234567890", "password", Instant.now());
+        final List<User> users = userService.findAll();
 
-        return ResponseEntity.ok().body(List.of(user));
+        return ResponseEntity.ok().body(users);
+    }
+
+    @RequestMapping(value="/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        final Optional<User> userOptional = userService.findById(id);
+
+        return ResponseEntity.ok(userOptional.get());
+//        return userOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
